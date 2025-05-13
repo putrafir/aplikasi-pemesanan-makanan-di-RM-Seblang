5d15d9de364cc8c9b3c5f5dd6abdbadf6c2ac8e0
arifahnbm <nura60583@gmail.com>
2025-05-13 09:22:59 +0700

    feat: menambahkan status dan memperbaiki error

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -4,63 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTransaksiRequest;
 use App\Http\Requests\UpdateTransaksiRequest;
+use Illuminate\Http\Request;
 use App\Models\Transaksi;
 
 class TransaksiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function updateStatus(Request $request, $id)
     {
-        //
-    }
+        $request->validate([
+            'status_baru' => 'required|in:aktif,nonaktif',
+        ]);
+        $transaksi = Transaksi::findOrFail($id);
+        $transaksi->status = $request->status_baru;
+        $transaksi->save();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return redirect()->back()->with('success', 'Status pesanan berhasil diperbarui.');
     }
+    
+    public function updateStatusBayar(Request $request, $id)
+    {
+        $request->validate([
+            'statusbayar_baru' => 'required|in:sudah bayar,belum bayar',
+        ]);
+        $transaksi = Transaksi::findOrFail($id);
+        $transaksi->status_bayar = $request->statusbayar_baru;
+        $transaksi->save();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTransaksiRequest $request)
-    {
-        //
+        return redirect()->back()->with('success', 'Status pembayaran berhasil diperbarui.');
     }
+    
+    
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Transaksi $transaksi)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Transaksi $transaksi)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTransaksiRequest $request, Transaksi $transaksi)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Transaksi $transaksi)
-    {
-        //
-    }
+    
 }
