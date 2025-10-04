5607b7323a5161dfcf6577660d84312520649db4
arifahnbm <nura60583@gmail.com>
2025-10-04 19:33:07 +0700

    feat: pesan lagi(pelanggan)

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::delete('/customer/keranjang/{id}', [KeranjangController::class, 'destroy'
 Route::post('/customer/keranjang/checkout', [KeranjangController::class, 'checkout'])->name('customer.keranjang.checkout');
 Route::put('/keranjang/{id}/update', [KeranjangController::class, 'update'])->name('customer.keranjang.update');
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -92,6 +93,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/kelolakasir', [KelolaKasirController::class, 'index'])->name('admin.kelolakasir');
     Route::post('/admin/kelolakasir/tambah', [KelolaKasirController::class, 'tambahkasir'])->name('admin.kelolakasir.tambah');
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/pesanan/{id}', [KeranjangController::class, 'detailPesanan'])->name('customer.detailPesanan');
+    Route::get('/riwayat/{nomor_meja}', [KeranjangController::class, 'riwayatPesanan'])->name('customer.riwayat');
 });
 
 
